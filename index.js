4c8105e6d74e975476eea90cb1baa4bd0a742794
syzer3@gmail.com <syzer3@gmail.com>
2017-08-26 14:46:01 +0200

    better view

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// Views is directory for all template files
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.get('/', (request, response) => {
+  response.render('pages/index')
+})
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'))
+})
