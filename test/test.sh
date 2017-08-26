bfc8902a1b2eba94da382ab6adcf2404db203304
syzer3@gmail.com <syzer3@gmail.com>
2017-08-26 14:48:35 +0200

    handeval

---
+++ test/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+// curl -XGET -d 'cards=[
+// {"rank":"5","suit":"diamonds"},
+// {"rank":"6","suit":"diamonds"},
+// {"rank":"7","suit":"diamonds"},
+// {"rank":"7","suit":"spades"},
+// {"rank":"8","suit":"diamonds"},
+// {"rank":"9","suit":"diamonds"}
+// ]' http://rainman.leanpoker.org/rank
